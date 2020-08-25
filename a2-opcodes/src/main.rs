1a0cf3734b62311d23dbc35d34bb2c9229877d7d
Nikita Barsukov <nbarsukov@ptsecurity.com>
2020-08-25 17:11:59 +0300

    A2: second star

--- a2-opcodes/src/main.rs
+++ a2-opcodes/src/main.rs
@@ -16,7 +16,29 @@ fn main() {
     computer.set_program(&program);
     computer.execute_program();
 
-    println!("First value: {:?}", computer.get_value_at_address(0));
+    println!("First star: {:?}", computer.get_value_at_address(0));
+
+    // Second star challenge
+    let required_output = 19690720;
+    let mut result = 0;
+    computer.reset();
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            program[1] = noun;
+            program[2] = verb;
+            computer.set_program(&program);
+            computer.execute_program();
+
+            let output = computer.get_value_at_address(0);
+            if output == required_output {
+                result = 100 * noun + verb;
+                break;
+            }
+        }
+    }
+
+    println!("Second star: {}", result);
 }
 
 fn read_program() -> Vec<usize> {
