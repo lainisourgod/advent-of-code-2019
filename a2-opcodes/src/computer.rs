267f34b79307c355e0cc2b6e67dfa779a20420e0
Nikita Barsukov <nbarsukov@ptsecurity.com>
2020-08-25 16:35:03 +0300

    Create Computer and Instruction structs

---
+++ a2-opcodes/src/computer.rs
@@ -0,0 +1,107 @@
+/// A basic unit of program.
+enum Instruction {
+    Add { a: usize, b: usize, to: usize },
+    Mul { a: usize, b: usize, to: usize },
+    Halt,
+}
+
+impl From<&[usize]> for Instruction {
+    /// A mapping from vector of values to Instruction for Computer.
+    fn from(params: &[usize]) -> Self {
+        use Instruction::*;
+        match params[0] {
+            1 => Add {
+                a: params[1],
+                b: params[2],
+                to: params[3],
+            },
+            2 => Mul {
+                a: params[1],
+                b: params[2],
+                to: params[3],
+            },
+            99 => Halt {},
+            _ => unreachable!(),
+        }
+    }
+}
+
+/// Computer stores a program in a memory as raw values. At each step computer reads some values
+/// from the memory at pointer, interprets it as an Instruction and executes mutating the memory.
+/// When an Instruction::Halt is fed, execution stops.
+pub struct Computer {
+    memory: Vec<usize>,
+    pointer: usize,
+}
+
+impl Computer {
+    pub fn new() -> Computer {
+        Computer {
+            memory: Vec::new(),
+            pointer: 0,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        *self = Computer::new();
+    }
+
+    pub fn set_program(&mut self, program: &[usize]) {
+        self.memory = Vec::from(program);
+        self.pointer = 0;
+    }
+
+    pub fn get_value_at_address(&self, address: usize) -> usize {
+        self.memory[address]
+    }
+
+    pub fn execute_program(&mut self) {
+        loop {
+            if self.pointer > self.memory.len() - 4 {
+                break;
+            }
+
+            let instruction = Instruction::from(&self.memory[self.pointer..self.pointer + 4]);
+            self.execute_instruction(&instruction);
+        }
+    }
+
+    /// Take an Instruction and mutate memory and pointer according to the rules of an instruction
+    /// execution.
+    fn execute_instruction(&mut self, instruction: &Instruction) {
+        use Instruction::*;
+
+        match instruction {
+            Halt => self.pointer = self.memory.len(),
+            Add { a, b, to } => {
+                self.memory[*to] = a + b;
+                self.pointer += 4;
+            }
+            Mul { a, b, to } => {
+                self.memory[*to] = a * b;
+                self.pointer += 4;
+            }
+        }
+    }
+}
+
+#[test]
+fn test_program_execution() {
+    let mut computer = Computer::new();
+
+    let cases: &[(&[usize], &[usize])] = &[
+        (&[1, 0, 0, 0, 99], &[2, 0, 0, 0, 99]),
+        (&[2, 3, 0, 3, 99], &[2, 3, 0, 6, 99]),
+        (&[2, 4, 4, 5, 99, 0], &[2, 4, 4, 5, 99, 9801]),
+        (
+            &[1, 1, 1, 4, 99, 5, 6, 0, 99],
+            &[30, 1, 1, 4, 2, 5, 6, 0, 99],
+        ),
+    ];
+
+    for case in cases {
+        computer.set_program(case.0);
+        computer.execute_program();
+        assert_eq!(computer.memory, case.1);
+    }
+}
--- a2-opcodes/src/main.rs
+++ a2-opcodes/src/main.rs
@@ -1,79 +1,29 @@
 use regex::Regex;
 use std::fs;
 
-type Program = Vec<usize>;
+mod computer;
+use computer::Computer;
 
 fn main() {
-    let program: Program = read_program();
+    let program: Vec<usize> = read_program();
     println!("The program: {:?}", program);
 
-    let new_program = execute_program(program);
-    println!("\nNew program: {:?}", new_program);
-    println!("First value: {:?}", new_program.get(0));
+    let mut computer = Computer::new();
+
+    computer.set_program(&program);
+    computer.execute_program();
+
+    println!("First value: {:?}", computer.get_value_at_address(0));
 }
 
-fn read_program() -> Program {
+fn read_program() -> Vec<usize> {
     let program_text = fs::read_to_string("program.txt").unwrap();
     let program_regex = Regex::new(r"(\d+)").unwrap();
 
-    let program: Program = program_regex
+    let program: Vec<usize> = program_regex
         .find_iter(&program_text)
         .map(|mat| mat.as_str().parse().unwrap())
         .collect();
 
     program
-}
-
-fn execute_program(mut program: Program) -> Program {
-    let mut result: usize;
-    let mut index: usize = 0;
-    loop {
-        if index > program.len() - 4 {
-            break;
-        }
-        let code_block: Vec<usize>;
-        {
-            code_block = Vec::from(&program[index..index + 4]);
-            match code_block[0] {
-                1 => {
-                    result = program[code_block[1]] + program[code_block[2]];
-                }
-                2 => {
-                    result = program[code_block[1]] * program[code_block[2]];
-                }
-                99 => break,
-                _ => panic!("Incorrect first code at block {:?}", code_block),
-            }
-        }
-        program[code_block[3]] = result;
-        index += 4;
-    }
-
-    program
-}
-
-#[test]
-fn test_program_execution() {
-    let cases = [
-        (
-            Program::from([1, 0, 0, 0, 99]),
-            Program::from([2, 0, 0, 0, 99]),
-        ),
-        (
-            Program::from([2, 3, 0, 3, 99]),
-            Program::from([2, 3, 0, 6, 99]),
-        ),
-        (
-            Program::from([2, 4, 4, 5, 99, 0]),
-            Program::from([2, 4, 4, 5, 99, 9801]),
-        ),
-        (
-            Program::from([1, 1, 1, 4, 99, 5, 6, 0, 99]),
-            Program::from([30, 1, 1, 4, 2, 5, 6, 0, 99]),
-        ),
-    ];
-
-    for case in &cases {
-        assert_eq!(execute_program(case.0.clone()), case.1)
-    }
 }
