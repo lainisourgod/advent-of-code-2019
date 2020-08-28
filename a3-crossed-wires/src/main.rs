7494296769ef5a118fe03df78183f003ba7125f5
Nikita Barsukov <nbarsukov@ptsecurity.com>
2020-08-28 18:22:49 +0300

    A3: add intersection finding and tests (not working)

--- a3-crossed-wires/src/main.rs
+++ a3-crossed-wires/src/main.rs
@@ -1,11 +1,60 @@
+use std::cmp::Ordering;
+use std::collections::HashSet;
 use std::fs;
+use std::ops::Add;
 
 fn main() {
-    let wires = read_wires();
-    println!("Wires: {:?}", wires);
+    let text = fs::read_to_string("input.txt").unwrap();
+    let wires = parse_wires(text);
+    for wire in &wires {
+        for point in wire {
+            println!("{:?}", point);
+        }
+    }
+    let a = &wires[0][0];
+    let b = &wires[0][1];
+    println!("a {:?} b {:?} a > b {}", a, b, a > b);
+    println!(
+        "Closes intersection: {:?}",
+        find_closest_intersection(&wires[0], &wires[1])
+    )
 }
 
-#[derive(Debug)]
+fn parse_wires(text: String) -> Vec<Wire> {
+    // Read moves
+    let moves_of_wires: Vec<Vec<Move>> = text
+        .trim()
+        .split("\n") // First split -- on two wires
+        .map(|wire_text| {
+            wire_text
+                .trim()
+                .split(",")
+                .map(|string| string.into())
+                .collect()
+        })
+        .collect();
+
+    let origin = Point { x: 0, y: 0 };
+
+    // Calculate every point of each wire
+    let wires: Vec<Wire> = moves_of_wires
+        .iter()
+        .map(|wire_moves| {
+            let mut current = origin.clone();
+            wire_moves
+                .iter()
+                .map(|&move_| {
+                    current = current + move_;
+                    current
+                })
+                .collect()
+        })
+        .collect();
+
+    wires
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -25,10 +74,10 @@ impl From<char> for Direction {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Move {
     direction: Direction,
-    distance: usize,
+    distance: u32,
 }
 
 impl From<&str> for Move {
@@ -40,22 +89,145 @@ impl From<&str> for Move {
     }
 }
 
-type Wire = Vec<Move>;
+type Wire = Vec<Point>;
 
-fn read_wires() -> Vec<Wire> {
-    let text = fs::read_to_string("input.txt").unwrap();
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
-    let wires: Vec<Wire> = text
-        .trim()
-        .split("\n") // First split -- on two wires
-        .map(|wire_text| {
-            wire_text
-                .trim()
-                .split(",")
-                .map(|string| string.into())
-                .collect()
-        })
-        .collect();
+impl Ord for Point {
+    /// Compare Points by Manhattan distance between a point and an origin (Point {x: 0, y: 0}).
+    /// Manhattan distance is sum of absolute values of every coordinate of the Point.
+    fn cmp(&self, other: &Self) -> Ordering {
+        // (self.x.abs() + self.y.abs()).cmp(&(other.x.abs() + other.y.abs()))
+        let a = self.x.abs() + self.y.abs();
+        let b = other.x.abs() + other.y.abs();
+        println!("CMP: a = {} b = {}", a, b);
+        a.cmp(&b)
+    }
+}
 
-    wires
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl Add<Move> for Point {
+    type Output = Self;
+
+    fn add(self, move_: Move) -> Self::Output {
+        match move_.direction {
+            Direction::Left => Point {
+                x: self.x - move_.distance as i32,
+                y: self.y,
+            },
+            Direction::Right => Point {
+                x: self.x + move_.distance as i32,
+                y: self.y,
+            },
+            Direction::Up => Point {
+                x: self.x,
+                y: self.y + move_.distance as i32,
+            },
+            Direction::Down => Point {
+                x: self.x,
+                y: self.y - move_.distance as i32,
+            },
+        }
+    }
+}
+
+fn find_closest_intersection(left: &[Point], right: &[Point]) -> Option<Point> {
+    let left_set: HashSet<Point> = left.into_iter().cloned().collect();
+    let right_set: HashSet<Point> = right.into_iter().cloned().collect();
+    left_set.intersection(&right_set).min().cloned()
+}
+
+#[test]
+fn test_find_closest_intersection() {
+    let cases = [
+        (
+            [
+                [Point { x: 0, y: 6 }, Point { x: 5, y: 0 }],
+                [Point { x: 5, y: 0 }, Point { x: 0, y: 6 }],
+            ],
+            Some(Point { x: 5, y: 0 }),
+        ),
+        (
+            [
+                [Point { x: 0, y: 6 }, Point { x: 5, y: 0 }],
+                [Point { x: 0, y: 6 }, Point { x: 5, y: 0 }],
+            ],
+            Some(Point { x: 5, y: 0 }),
+        ),
+    ];
+
+    for case in &cases {
+        assert_eq!(
+            find_closest_intersection(&case.0[0], &case.0[1]),
+            Some(Point { x: 5, y: 0 })
+        )
+    }
+}
+
+#[test]
+fn test_point_ordering() {
+    let point1 = Point { x: 0, y: 6 };
+    let point2 = Point { x: 5, y: 0 };
+    assert_eq!(point1 > point2, true);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: -3, y: 6 };
+    let point2 = Point { x: 5, y: 0 };
+    assert_eq!(point1 > point2, true);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: 0, y: 6 };
+    let point2 = Point { x: -5, y: 0 };
+    assert_eq!(point1 > point2, true);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: 0, y: -6 };
+    let point2 = Point { x: 5, y: 0 };
+    assert_eq!(point1 > point2, true);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: 5, y: 5 };
+    let point2 = Point { x: 5, y: 5 };
+    assert_eq!(point1 > point2, false);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: -10, y: -10 };
+    let point2 = Point { x: 5, y: 0 };
+    assert_eq!(point1 > point2, true);
+    assert_eq!(point1 < point2, false);
+
+    let point1 = Point { x: 0, y: 3 };
+    let point2 = Point { x: 5, y: 2 };
+    assert_eq!(point1 > point2, false);
+    assert_eq!(point1 < point2, true);
+}
+
+#[test]
+fn test_wire_parsing() {
+    let text = "R992,U221,L822,U805\nL998,U308,R889".to_owned();
+    assert_eq!(
+        parse_wires(text),
+        [
+            Vec::from([
+                Point { x: 992, y: 0 },
+                Point { x: 992, y: 221 },
+                Point { x: 170, y: 221 },
+                Point { x: 170, y: 1026 }
+            ]),
+            Vec::from([
+                Point { x: -998, y: 0 },
+                Point { x: -998, y: 308 },
+                Point { x: -109, y: 308 }
+            ])
+        ]
+    )
 }
